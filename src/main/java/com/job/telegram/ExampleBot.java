65903b446d85e7e9ca34ff7a36ca63a3b7a54aa9
Bygaga63 <bygaga63@gmail.com>
2019-01-15 10:42:12 +0300

    first commit

---
+++ src/main/java/com/job/telegram/ExampleBot.java
@@ -0,0 +1,65 @@
+package com.job.telegram;
+
+
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class ExampleBot extends TelegramLongPollingBot {
+
+    public ExampleBot(String botToken, String botUsername, DefaultBotOptions options) {
+        super(options);
+        this.BOT_TOKEN = botToken;
+        this.BOT_NAME = botUsername;
+    }
+
+    private final String BOT_TOKEN;
+    private final String BOT_NAME;
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Катя я люблю тебя");
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+}
---
+++ src/main/java/com/job/telegram/TelegramApplication.java
@@ -0,0 +1,49 @@
+package com.job.telegram;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+
+@SpringBootApplication
+public class TelegramApplication implements CommandLineRunner {
+    @Value("${bot.token}")
+    private String BOT_TOKEN = "720900952:AAHGWgJd4AedPMeS8ZKCInoVT2_8Cv5QCuM";
+    @Value("${bot.username}")
+    private String BOT_NAME  = "JOIN TO PARTY";
+    @Value("${proxy.host}")
+    private  String PROXY_HOST = "127.0.0.1";
+    @Value("${proxy.port}")
+    private  Integer PROXY_PORT = 	9150;
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        ApiContextInitializer.init();
+
+        // Create the TelegramBotsApi object to register your bots
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Set up Http proxy
+        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+
+        botOptions.setProxyHost(PROXY_HOST);
+        botOptions.setProxyPort(PROXY_PORT);
+        // Select proxy type: [HTTP|SOCKS4|SOCKS5] (default: NO_PROXY)
+        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
+
+        // Register your newly created AbilityBot
+        ExampleBot bot = new ExampleBot(BOT_TOKEN, BOT_NAME, botOptions);
+
+        botsApi.registerBot(bot);
+
+    }
+}
+
